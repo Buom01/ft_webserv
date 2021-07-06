7d437219d2dac4bf7e8020c48191828b76559d2c
Bastien Adam <badam@student.42.fr>
2021-07-06 23:29:49 +0200

    Add warning and error functions

--- srcs/log/Log.hpp
+++ srcs/log/Log.hpp
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 16:28:32 by badam             #+#    #+#             */
-/*   Updated: 2021/07/06 16:34:30 by badam            ###   ########.fr       */
+/*   Updated: 2021/07/06 23:27:59 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define LOG_HPP
 
 # define COLOR_RESET ""
+# define COLOR_ERROR ""
+# define COLOR_WARNING ""
 # define COLOR_SERVER_ERROR ""
 # define COLOR_CLIENT_ERROR ""
 # define COLOR_REDIRECT ""
--- srcs/log/Log.test.cpp
+++ srcs/log/Log.test.cpp
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 16:08:34 by badam             #+#    #+#             */
-/*   Updated: 2021/07/06 16:27:57 by badam            ###   ########.fr       */
+/*   Updated: 2021/07/06 23:27:07 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,6 @@ int	main(void)
 	logger.log(404, "/not-found/lost", "File has been lost");
 	logger.log(500, "/server-error");
 	logger.log(500, "/server-error/oops", "Oops!");
+	logger.warn("Simple warn");
+	logger.fail("Critical error");
 }
