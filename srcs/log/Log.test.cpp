632238e8fc8cb947fb88de09ebf1a510f6bf45b2
Bastien Adam <badam@student.42.fr>
2021-07-06 16:38:28 +0200

    Add logging module

---
+++ srcs/log/Log.test.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Log.test.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 16:08:34 by badam             #+#    #+#             */
+/*   Updated: 2021/07/06 16:27:57 by badam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Log.hpp"
+
+
+int	main(void)
+{
+	Log	logger = Log();
+
+	logger.greeting("localhost", 80);
+	logger.log(200, "/found");
+	logger.log(200, "/found/yay", "Yay!");
+	logger.log(404, "/not-found");
+	logger.log(404, "/not-found/lost", "File has been lost");
+	logger.log(500, "/server-error");
+	logger.log(500, "/server-error/oops", "Oops!");
+}
