ade064de22e6e176c721ffe4e25005e135b0ec89
Bastien ADAM <badam@student.42.fr>
2022-02-17 17:22:20 +0100

    Make breakable chain's middleware working with epoll

--- srcs/includes/Request.hpp
+++ srcs/includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 12:27:56 by badam             #+#    #+#             */
-/*   Updated: 2022/01/11 13:30:03 by badam            ###   ########.fr       */
+/*   Updated: 2022/02/17 16:03:24 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,19 @@
 # include "Header.hpp"
 # include "http.hpp"
 
+typedef enum	chain_state_e
+{
+	CS_OTHER = 0,
+	CS_AWAIT_EVENT
+}				chain_state_t;
+
 class Request
 {
 	public:
 		clock_t				start;
-		server_bind_t		bind;
 		int					fd;
+		uint32_t			events;
+		chain_state_t		*state;
 		char				buff[SERVER_BUFFER_SIZE];
 		
 		method_t			method;
@@ -34,6 +41,8 @@ class Request
 		Request() :
 			start(clock()),
 			fd(0),
+			events(0),
+			state(NULL),
 			method(M_UNKNOWN),
 			pathname(""),
 			trusted_pathname(""),
@@ -41,7 +50,6 @@ class Request
 			headers()
 		{
 			bzero(&buff, sizeof(buff));
-			bzero(&bind, sizeof(bind));
 		}
 
 		Request 	(const Request &rhs)
@@ -49,10 +57,11 @@ class Request
 			*this = rhs;
 		}
 
-		Request(int connection, server_bind_t &binding) :
+		Request(int connection, uint32_t _events) :
 			start(clock()),
-			bind(binding),
 			fd(connection),
+			events(_events),
+			state(NULL),
 			method(M_UNKNOWN),
 			pathname(""),
 			trusted_pathname(""),
@@ -70,8 +79,8 @@ class Request
 			if (this != &rhs)
 			{
 				start	= rhs.start;
-				bind	= rhs.bind;
 				fd		= rhs.fd;
+				events	= rhs.events;
 				memcpy(buff, rhs.buff, SERVER_BUFFER_SIZE);
 
 				method				= rhs.method;
@@ -84,6 +93,38 @@ class Request
 			return (*this);
 		}
 
+		bool	await(uint32_t _events)
+		{
+			if (events & _events)
+			{
+				*state = CS_OTHER;
+				return (true);
+			}
+			else
+			{
+				std::cout << "Await..." << std::endl;
+				events = events | _events;
+				*state = CS_AWAIT_EVENT;
+				return (false);
+			}
+		}
+
+		void	fire(uint32_t _events)
+		{
+			std::cout << "Fire !" << std::endl;
+			events = events | _events;
+		}
+
+		void	unfire(uint32_t _events)
+		{
+			std::cout << "Unfire !" << std::endl;
+			events = events &~ _events;
+		}
+
+		bool	closed()
+		{
+			return (events & EPOLLHUP);
+		}
 };
 
 #endif
--- srcs/serve/Chain.hpp
+++ srcs/serve/Chain.hpp
@@ -6,7 +6,7 @@
 /*   By: badam <badam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 16:19:54 by badam             #+#    #+#             */
-/*   Updated: 2022/01/27 00:13:24 by badam            ###   ########.fr       */
+/*   Updated: 2022/02/17 02:50:36 by badam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,19 @@ class	RunningChain
 	public:
 		Request 			req;
 		Response			res;
+		uint32_t			events;
 		chain_t::iterator	pos;
+		chain_state_t		state;
 
-		RunningChain(Request &_req, Response &_res, chain_t::iterator _pos):
+		RunningChain(Request &_req, Response &_res, uint32_t _events, chain_t::iterator _pos):
 			req(_req),
 			res(_res),
-			pos(_pos)
-		{}
+			events(_events),
+			pos(_pos),
+			state(CS_OTHER)
+		{
+			req.state = &state;
+		}
 };
 
 typedef std::vector<RunningChain *>    running_chains_t;
@@ -95,35 +101,65 @@ class   Chain
 			{
 				return _run(instance);
 			}
-			// catch (const std::exception &e)
-			// {
-			// 	instance.res.logger->fail(e.what());
-			// 	instance.res.error = &e;
+			catch (const std::exception &e)
+			{
+				instance.state = CS_OTHER;
+				instance.res.logger->fail(e.what());
+				instance.res.error = &e;
 
-			// 	try
-			// 	{
-			// 		if (_error_chain)
-			// 			_error_chain->exec(instance.req, instance.res);  // Should replace the running instance
-			// 		else
-			// 			throw (CantHandleRequest());
-			// 	}
-			// 	catch(const std::exception &ce)
-			// 	{
-			// 		instance.res.logger->fail("Failed to answer");
+				// try
+				// {
+				// 	if (_error_chain)
+				// 	{
+				// 		_error_chain->exec(instance.req, instance.res, instance.events);  // Should replace the running instance. Verify that's the case
+				// 		return (true);
+				// 	}
+				// 	else
+				// 		throw (CantHandleRequest());
+				// }
+				// catch(const std::exception &ce)
+				// {
+					instance.res.logger->fail("Failed to answer");
 
-			// 		try
-			// 		{
-			// 			::close(instance.res.fd);
-			// 		}
-			// 		catch (...)
-			// 		{}
-			// 	}
-			// }
+					try
+					{
+						::close(instance.res.fd);
+					}
+					catch (...)
+					{}
+
+					return (true);
+				// }
+			}
 			catch (...)
 			{
 				instance.res.logger->fail("Unhandled exception");
 
+				try
+				{
+					::close(instance.res.fd);
+				}
+				catch(...)
+				{}
+
 				return (true);
+			}
+		}
+
+		void _remove_instance(RunningChain *instance)
+		{
+			running_chains_t::iterator	it	= _running.begin();
+
+			while (it != _running.end())
+			{
+				if (*it == instance)
+				{
+					delete *it;
+					it = _running.erase(it);
+					// @TODO: Remove FD from poll
+				}
+				else
+					++it;
 			}
 		}
     
@@ -165,25 +201,77 @@ class   Chain
             _raw_chain.push_back(link);
 		}
 		
-        void	exec(int connection, server_bind_t bind, Log &logger)
+        RunningChain	*exec(int connection, uint32_t events, Log &logger)
         {
-			Request			req(connection, bind);
+			Request			req(connection, events);
 			Response		res(connection, logger);
-            RunningChain	*instance	= new RunningChain(req, res, _raw_chain.begin());
-
-			if (!_exec_instance(*instance))
-				_running.push_back(instance);
-        }
-		
-        void	exec(Request &req, Response &res)
-        {
-            RunningChain	*instance	= new RunningChain(req, res, _raw_chain.begin());
+            RunningChain	*instance	= new RunningChain(req, res, events, _raw_chain.begin());
 
 			if (!_exec_instance(*instance))
 				_running.push_back(instance);
 			else
+			{
 				delete (instance);
+				instance = NULL;
+			}
+
+			return (instance);
         }
+		
+        // void	exec(Request &req, Response &res, uint32_t events)
+        // {
+        //     RunningChain	*instance	= new RunningChain(req, res, events, _raw_chain.begin());
+
+		// 	if (!_exec_instance(*instance))
+		// 	{
+		// 		_running.push_back(instance);
+		// 		// @TODO: add FD to poll
+		// 	}
+		// 	else
+		// 		delete (instance);
+        // }
+
+		bool	retake(RunningChain *instance, uint32_t events)
+		{
+			instance->req.fire(events);
+			if (instance->state == CS_AWAIT_EVENT)
+			{
+				instance->state = CS_OTHER;
+				
+				if (_exec_instance(*instance))
+				{
+					_remove_instance(instance);
+					return (true);
+				}
+			}
+			return (false);
+		}
+
+		// bool	retake(int fd, uint32_t events)
+		// {
+		// 	// Should also support POLLHUP for closed streams
+		// 	running_chains_t::iterator	it	= _running.begin();
+
+		// 	while (it != _running.end())
+		// 	{
+		// 		if ((*it)->req.fd == fd)
+		// 		{
+		// 			if (((*it)->state == CS_AWAIT_IN && ))
+		// 			{
+		// 				if (_exec_instance(**it))
+		// 				{
+		// 					delete *it;
+		// 					_running.erase(it);
+		// 					// Remove FD from poll
+		// 				}
+		// 			}
+		// 			return (true);
+		// 		}
+		// 		++it;
+		// 	}
+			
+		// 	return (false);
+		// }
 
 		void	retake()
 		{
@@ -191,10 +279,11 @@ class   Chain
 
 			while (it != _running.end())
 			{
-				if (_exec_instance(**it))
+				if ((*it)->state == CS_OTHER && _exec_instance(**it))
 				{
 					delete *it;
 					it = _running.erase(it);
+					// @TODO: Remove FD from poll
 				}
 				else
 					++it;
