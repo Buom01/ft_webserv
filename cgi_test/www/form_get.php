99fb56024bf27e60277b6c4c7a35d71547adc96d
Bastien ADAM <badam@e2r12p4.clusters.42paris.fr>
2022-05-03 21:06:02 +0200

    Cleanup unused tests

--- cgi_test/www/form_get.php
+++
@@ -1,8 +0,0 @@
-<?php
-	echo "<meta charset=\"UTF-8\">";
-	$nom = $_GET['nom'];
-	$prenom = $_GET['prenom'];
-	echo "GET form";
-	echo "<h3>Bienvenue à ".$prenom.' '.$nom,"</h3>";
-	echo "<p><a href='index.php'>Retour au formulaire</a></p>";
-?>
